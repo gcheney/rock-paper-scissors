c32e36468b5696ca8e8759e779e553c4353e2769
Glendon Cheney <glendon.cheney@gmail.com>
2016-06-15 12:15:04 -0500

    update img folder

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -15,7 +15,7 @@ namespace RockPaperScissors
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainUI());
         }
     }
 }
